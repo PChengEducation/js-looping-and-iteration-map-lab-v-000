f23ee557a1c77fa32bc47ea2fdf4e3eb6242d158
Pan Cheng <pchengeducation@hotmail.com>
2018-08-08 13:39:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  const lowerCased = drivers.map(function(name){
+    return name.toLowerCase();
+  });
+  return lowerCased;
+}
